7f0363e5041aadd8e175db7f95048e8ef1db8cc2
Jose Medrano <josmed@microsoft.com>
2021-10-14 11:21:51 +0200

    net project

---
+++ src/Controls/samples/Maui.Controls.Sample.Macos/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Maui.Controls.Sample.Macos
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
